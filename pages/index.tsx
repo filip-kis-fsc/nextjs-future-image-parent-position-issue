87d3624c8114382b00c376d6201813203d504b9f
Filip Kis <filip.kis@firesidechat.com>
2022-09-21 16:54:40 +0200

    Add example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,19 @@
-/** Add your relevant code here for the issue to reproduce */
+import Image from "next/future/image";
+import { useEffect, useState } from "react";
+import testImage from "/public/test-image.jpg";
+
 export default function Home() {
-  return null
+  const [displayImage, setDisplayImage] = useState(true);
+
+  useEffect(() => {
+    setDisplayImage(false);
+  }, []);
+
+  return (
+    displayImage && (
+      <div style={{ position: "relative", width: 10, height: 10 }}>
+        <Image priority fill src={testImage} alt="" sizes="10px" />
+      </div>
+    )
+  );
 }
